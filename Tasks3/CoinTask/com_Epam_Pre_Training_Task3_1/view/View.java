150fb0d136198b878f99822de546987b41a309c3
Mike <cosnmiir@gmail.com>
2018-02-07 13:06:27 +0300

    -

--- Tasks3/CoinTask/com_Epam_Pre_Training_Task3_1/view/View.java
+++
@@ -1,7 +0,0 @@
-package com_Epam_Pre_Training.Task3.view;
-
-public class View {
-	public static void prtinEagles(int quantityToss, int eagles) {
-		System.out.println("В " + quantityToss + " бросках выпал орел");
-	}
-}
